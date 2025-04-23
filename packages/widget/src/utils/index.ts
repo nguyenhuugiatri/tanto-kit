37665e5239db897106f7421518483531a92a3ef8
nguyenhuugiatri <nguyenhuugiatri@gmail.com>
2025-04-23 16:19:49 +0700

    feat: update connect button

--- packages/widget/src/utils/index.ts
+++ packages/widget/src/utils/index.ts
@@ -56,7 +56,15 @@ export const isRoninExtensionInstalled = (connectors: readonly Connector<CreateC
   return connectors.some(connector => connector.id === 'com.roninchain.wallet');
 };
 
-export const truncate = (value?: string, prefixChar = 8, suffixChar = 6, bridge = '...') => {
+export const truncate = (
+  value?: string,
+  options?: {
+    prefixChar?: number;
+    suffixChar?: number;
+    bridge?: string;
+  },
+) => {
+  const { prefixChar = 8, suffixChar = 6, bridge = '•••' } = options ?? {};
   if (!value) return '';
   if (value.length <= prefixChar + suffixChar + bridge.length) return value;
   return `${value.slice(0, prefixChar)}${bridge}${value.slice(-suffixChar)}`;
